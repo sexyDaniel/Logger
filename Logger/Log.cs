d458cf7d087762b7e70d7a657076cbbd4e7c82a3
Danil <danil.lazarev.90@mail.ru>
2021-03-20 21:58:03 +0300

    End

---
+++ Logger/Log.cs
@@ -0,0 +1,156 @@
+﻿using PractLogger.Logger.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PractLogger.Logger
+{
+    public class Log : ILog
+    {
+        private string todayCurrentDir;
+        public Log() 
+        {
+            todayCurrentDir = Directory.GetCurrentDirectory() + "/" + DateTime.Now.ToString("dd-MM-yy");
+            CreateTodayFolder(todayCurrentDir);
+        }
+        public void Debug(string message)
+        {
+            LogWrite(LogType.Debug, message);
+        }
+
+        public void Debug(string message, Exception e)
+        {
+            string printMessage = $" {message}; EXСEPTION : {e.Message}";
+            LogWrite(LogType.Debug, printMessage);
+        }
+
+        public void DebugFormat(string message, params object[] args)
+        {
+            string printMessage = $" {message}; args-{String.Join(";", args)}";
+            LogWrite(LogType.Debug, printMessage);
+        }
+
+        public void Error(string message)
+        {
+            LogWrite(LogType.Error, " "+message);
+        }
+
+        public void Error(string message, Exception e)
+        {
+            string printMessage = $" {message}; EXСEPTION - {e.Message}";
+            LogWrite(LogType.Error, printMessage);
+        }
+
+        public void Error(Exception ex)
+        {
+            string printMessage = $" EXСEPTION - {ex.Message}";
+            LogWrite(LogType.Error, printMessage);
+        }
+
+        public void ErrorUnique(string message, Exception e)
+        {
+            string printMessage = $" {message}; EXСEPTION - {e.Message}";
+            if(!CheckUnique(LogType.Error,printMessage))
+                LogWrite(LogType.Error, printMessage);
+        }
+
+        public void Fatal(string message)
+        {
+            string printMessage = $" {message}";
+            LogWrite(LogType.Fatal, printMessage);
+        }
+
+        public void Fatal(string message, Exception e)
+        {
+            string printMessage = $" {message}; EXСEPTION : {e.Message}";
+            LogWrite(LogType.Fatal, printMessage);
+        }
+
+        public void Info(string message)
+        {
+            LogWrite(LogType.Info, " " + message);
+        }
+
+        public void Info(string message, Exception e)
+        {
+            string printMessage = $" {message}; EXСEPTION : {e.Message}";
+            LogWrite(LogType.Info, printMessage);
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            string printMessage = $" {message}; args-{String.Join(";", args)}";
+            LogWrite(LogType.Info, printMessage);
+        }
+
+        public void SystemInfo(string message, Dictionary<object, object> properties = null)
+        {
+            LogWrite(LogType.SystemInfo, message, properties);
+        }
+
+        public void Warning(string message)
+        {
+            LogWrite(LogType.Warning, " " + message);
+        }
+
+        public void Warning(string message, Exception e)
+        {
+            string printMessage = $" {message}; EXСEPTION : {e.Message}";
+            LogWrite(LogType.Warning, printMessage);
+        }
+
+        public void WarningUnique(string message)
+        {
+            string printMessage = $" {message}";
+            if (!CheckUnique( LogType.Warning,printMessage))
+                LogWrite(LogType.Warning, printMessage);
+        }
+
+        private void CreateTodayFolder(string currentDir) 
+        {
+            Directory.CreateDirectory(currentDir);
+        }
+
+        private void LogWrite(LogType logType, string message, Dictionary<object, object> properties = null) 
+        {
+            using (StreamWriter writer = new StreamWriter($"{todayCurrentDir}/{logType}.log", true))
+            {
+                writer.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {logType} :{message}");
+                if (properties != null) 
+                {
+                    foreach (var key in properties.Keys)
+                    {
+                        writer.WriteLine($"\t{key}-{properties[key]}");
+                    }
+                }
+            }
+        }
+
+        private bool CheckUnique(LogType logType,string checkMessage) 
+        {
+            using (StreamReader reader = new StreamReader($"{todayCurrentDir}/{logType}.log")) 
+            {
+                while (!reader.EndOfStream) 
+                {
+                    var parseMess = reader.ReadLine().Split(":");
+                    if (parseMess.Last().Equals(checkMessage)) 
+                    {
+                        reader.Close();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private enum LogType
+        {
+            Debug,
+            Info,
+            Warning,
+            Fatal,
+            Error,
+            SystemInfo
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using PractLogger.Logger;
+
+namespace PractLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log logger = new Log();
+            logger.Info("Запуск программы");
+            logger.Info("Обнаружилась ошибка",new Exception("Что-то неверно"));
+            logger.Info("Обнаружилась ошибка",1,2,3,4,5,6,7);
+            logger.Debug("для вывода информации, которая может быть полезной в процессе разработки и отладки приложения");
+            logger.Debug("для вывода информации, которая ...", new Exception("NullRef"));
+            logger.DebugFormat("для вывода информации, которая ...", 5,6,7,8,9,0);
+            logger.Error("Обнаружен Error");
+            logger.Error("Обнаружен Error с ошибкой",new Exception("NullRef"));
+            logger.ErrorUnique("Обнаружен Error с ошибкой", new Exception("NullRef"));
+            logger.ErrorUnique("Обнаружен Error с ошибкой", new Exception("OutOfRange"));
+            logger.Fatal("Обнаружен фатал");
+            logger.Fatal("Обнаружен фатал", new Exception("NullRef"));
+            logger.Fatal("Обнаружен фатал1");
+            logger.Warning("Обнаружен варнинг");
+            logger.Warning("Обнаружен варнинг",new Exception("NullRef"));
+            logger.WarningUnique("Обнаружен варнинг");
+            logger.WarningUnique("Обнаружен варнинг1");
+            logger.SystemInfo("Системная информация",new Dictionary<object, object>{
+                { 1,"первое сообщение"},
+                { 2,"второе сообщение"},
+                { 3,"третье сообщение"}
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
